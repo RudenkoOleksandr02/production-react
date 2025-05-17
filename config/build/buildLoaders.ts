76209cfb911f326dd0bdf0936ea7930234d84807
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-05-17 17:06:53 +0300

    [optional] add babel & extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -42,7 +42,28 @@ export function buildLoaders({isDev}: BuildOptions): webpack.RuleSetRule[] {
         exclude: /node_modules/,
     }
 
+    const babelLoader = {
+        test: /\.(js|jsx|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: "babel-loader",
+            options: {
+                presets: ['@babel/preset-env'],
+                plugins: [
+                    [
+                        "i18next-extract",
+                        {
+                            locales: ['en', 'ua', 'ru'],
+                            keyAsDefaultValue: true
+                        }
+                    ],
+                ]
+            }
+        }
+    }
+
     return [
+        babelLoader,
         typescriptLoader,
         cssLoader,
         svgLoader,
