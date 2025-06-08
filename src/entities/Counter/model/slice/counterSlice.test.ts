c6e650682b14d26e18b319c85b13ab11b3aa6b79
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-06-09 00:04:24 +0300

    add redux store & entity

---
+++ src/entities/Counter/model/slice/counterSlice.test.ts
@@ -0,0 +1,22 @@
+import { CounterSchema } from '../types/counterSchema';
+import { CounterReducer, CounterActions } from './counterSlice';
+
+describe('counterSlice.test', () => {
+    test('decrement', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(
+            CounterReducer(state, CounterActions.decrement()),
+        ).toEqual({ value: 9 });
+    });
+    test('increment', () => {
+        const state: CounterSchema = { value: 10 };
+        expect(
+            CounterReducer(state, CounterActions.increment()),
+        ).toEqual({ value: 11 });
+    });
+    test('should work with empty state', () => {
+        expect(
+            CounterReducer(undefined, CounterActions.increment()),
+        ).toEqual({ value: 1 });
+    });
+});
