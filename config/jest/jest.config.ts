135c6d55c5c04707ca51a269f81938a5d2bd74f9
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-08-01 00:29:54 +0300

    add alias for jest config

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -38,6 +38,8 @@ export default {
     moduleNameMapper: {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
+        '^@/(.*)$': '<rootDir>/src/$1',
+
     },
     reporters: [
         'default',
