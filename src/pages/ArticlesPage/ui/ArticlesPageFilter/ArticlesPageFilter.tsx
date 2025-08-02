d44ccb139ba48e3350d2a47a0ccfc2716ee0d654
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-08-03 02:35:42 +0300

    refactor public api for shared ui

--- src/pages/ArticlesPage/ui/ArticlesPageFilter/ArticlesPageFilter.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPageFilter/ArticlesPageFilter.tsx
@@ -7,8 +7,8 @@ import {
     ArticleSortField, ArticleSortSelector, ArticleType, ArticleView, ArticleTypeTabs,
 } from '@/entities/Article';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
-import { Card } from '@/shared/ui/Card/Card';
-import { Input } from '@/shared/ui/Input/Input';
+import { Card } from '@/shared/ui/Card';
+import { Input } from '@/shared/ui/Input';
 import { SortOrder } from '@/shared/types';
 import { useDebounce } from '@/shared/lib/hooks/useDebounce/useDebounce';
 import { articlesPageActions } from '../../model/slice/articlesPageSlice';
