36b8495c637bc4761376e5e9432f7d098426d766
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-08-09 21:00:52 +0300

    update own plugin 2

--- src/app/providers/router/ui/AppRouter.tsx
+++ src/app/providers/router/ui/AppRouter.tsx
@@ -1,7 +1,7 @@
 import React, { memo, Suspense, useCallback } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { PageLoader } from '@/widgets/PageLoader';
-import { RequireAuth } from '../ui/RequireAuth';
+import { RequireAuth } from './RequireAuth';
 import { routeConfig } from '../config/routeConfig';
 import { AppRoutesProps } from '@/shared/types/router';
 
