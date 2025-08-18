cbcdba6185f523268fced2e9dafe4ec49a23f032
RudenkoOleksandr02 <gitemail02@gmail.com>
2025-08-18 17:20:35 +0300

    add greeting modal for articles page

--- src/entities/User/model/types/jsonSettings.ts
+++ src/entities/User/model/types/jsonSettings.ts
@@ -2,6 +2,5 @@ import { Theme } from '@/shared/const/theme';
 
 export interface JsonSettings {
     theme?: Theme;
-    isVisitFirst?: true;
-    settingsPageHasBeenOpen?: false;
+    isArticlesPageHasBeenOpen?: boolean;
 }
